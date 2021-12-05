3c1d1789837f62acd6658832d292a4ff840bfb9e
metinozkan <metin.ozkn@hotmail.com>
2021-12-05 22:28:24 +0300

    added mantine libs

---
+++ source/components/TeamCard.tsx
@@ -0,0 +1,16 @@
+import { Grid, Image } from "@mantine/core";
+import React from "react";
+
+const TeamCard = () => {
+  return (
+    <Grid>
+      <Image
+        width={200}
+        height={80}
+        src="https://images.unsplash.com/long-image-url-was-here.jpg"
+      />
+    </Grid>
+  );
+};
+
+export default TeamCard;
